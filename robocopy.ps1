f5a823daa6c804c32fa30ee4215168a2c01514d1
jordan78 <monther.yasin@gmail.com>
2018-10-24 10:56:23 -0400

    Create robocopy.ps1

---
+++ robocopy.ps1
@@ -0,0 +1,10 @@
+$src = "\\sourcefileserver\homedir"
+$dest = "\\destfileserver\homedir"
+
+$option = "/E /COPYALL /DCOPY:DAT /MIR /MT:96"
+
+## loop through some list of user names 
+
+foreach($user in get-content c:\listofusers.txt) { 
+    write-host "Robocopy $src\$user $dest\$user $option /Log:c:\log_$user.txt" 
+}
