248af49aa60ceef3032c6bd109d063f6e2a0e05c
jordan78 <monther.yasin@gmail.com>
2018-10-24 11:08:22 -0400

    Update robocopy.ps1

--- robocopy.ps1
+++ robocopy.ps1
@@ -6,5 +6,6 @@ $option = "/E /COPYALL /DCOPY:DAT /MIR /MT:96"
 ## loop through some list of user names 
 
 foreach($user in get-content c:\listofusers.txt) { 
-    write-host "Robocopy $src\$user $dest\$user $option /Log:c:\log_$user.txt" 
+    #write-host "Robocopy $src\$user  $dest\$user $option /Log:c:\log_$user.txt"
+    Powershell -Command "Robocopy $src\$user  $dest\$user $option /Log:c:\log_$user.txt"
 }
