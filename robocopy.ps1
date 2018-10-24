6c4dcd93f735dbf4d64e2b686bcb5599c5f10cf3
jordan78 <monther.yasin@gmail.com>
2018-10-24 13:05:21 -0400

    Update robocopy.ps1

--- robocopy.ps1
+++ robocopy.ps1
@@ -8,6 +8,6 @@ $option = "/E /COPYALL /DCOPY:DAT /MIR /MT:96 /r:5 /w:10"
 foreach($user in get-content c:\listofusers.txt) { 
     #write-host "Robocopy $src\$user  $dest\$user $option /Log:c:\log_$user.txt"
     # create homedir first 
-    New-Item -ItemType directory -Path $src\$user
+    New-Item -ItemType directory -Path $dest\$user
     Powershell -Command "Robocopy $src\$user  $dest\$user $option /Log:c:\log_$user.txt"
 }
